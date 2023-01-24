a0c9c2d7cf9e6725630f1181f2f129542a636b24
SadovayaSofiya <sadovaya.sofia666@gmail.com>
2023-01-24 16:29:13 +0300

    init

---
+++ cmd/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"repo-for-cloning/internal/math"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	fmt.Println(math.Cosinus(10))
+}
---
+++ internal/math/math.go
@@ -0,0 +1,19 @@
+package math
+
+func Sinus(input float64) (output float64) {
+	// Insert your code here
+
+	return 0.0
+}
+
+func Cosinus(input float64) (output float64) {
+	// Insert your code here
+
+	return 0.0
+}
+
+func Tangens(input float64) (output float64) {
+	// Insert your code here
+
+	return 0.0
+}
